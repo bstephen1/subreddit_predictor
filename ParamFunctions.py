584f984d5236176973a32d4ac99af346caa907b7
edaniel1 <edaniel1@umbc.edu>
2017-04-30 15:15:17 -0400

    Fixed Errors in the Functions
    Fixed Errors in the Functions that caused them to not be able to run or function properly.
    Must install textblob for the functions to work.
    Must have done the extra install "python -m textblob.download_corpora" for it to work.

--- ParamFunctions.py
+++ ParamFunctions.py
@@ -1,28 +1,33 @@
 from textblob import TextBlob
 
+# python -m textblob.download_corpora
+
+
+
 
 #Input:
-#	an list of strings to find avg sentiment of
+#	a list of strings to find the average sentiment of
 #Return:
-#	a tuple of the average sentiment formatted as (polarity, subjectivity)
+#	a list of the average sentiment formatted as (polarity, subjectivity)
 def getAvgSentiment(testList):
 	
-	avgSentiment = (0,0)
+	avgSentiment = [0,0]
 	
 	for testStr in testList:
 		testSentiment = getSentiment(testStr)
 		avgSentiment[0] += testSentiment[0]
 		avgSentiment[1] += testSentiment[1]
 	
-	avgSentiment = avgSentiment/len(testList)
+	avgSentiment[0] = avgSentiment[0]/len(testList)
+	avgSentiment[1] = avgSentiment[1]/len(testList)
 	return avgSentiment
 
 #Input:
 #	a string to find the avg sentiment of
 #Return:
-#	a tuple of the sentiment formatted as (polarity, subjectivity)
+#	a list of the sentiment formatted as (polarity, subjectivity)
 def getSentiment(testStr):
-	return TextBlob(testStr).sentiment
+	return list(TextBlob(testStr).sentiment)
 
 #Input:
 #	an int n that denotes the value for n-grams
@@ -45,7 +50,7 @@ def getAllAvgRougeN(n, testList, summaryList):
 def getAvgRougeN(n, testList, summary):
 	nGramSummary = []
 	for item in summary:
-		nGramSummary.extend(TextBlob(item).grams(n))
+		nGramSummary.extend(TextBlob(item).ngrams(n))
 	avgRougeN = 0
 	for testStr in testList:
 		avgRougeN += getRougeN(n, testStr, nGramSummary)
@@ -75,11 +80,11 @@ def getRougeN(n, testStr, nGramSummary):
 def getAllAvgComSubstrCount(n, testList, summaryList):
 	allAvgComSubstrCount = []
 	for summary in summaryList:
-		allAvgComSubstrCount.append(n, getAvgRougeN(testList, summary))
+		allAvgComSubstrCount.append(getAvgComSubstrCount(n, testList, summary))
 	return allAvgComSubstrCount
 
 #Input:
-#	an int n that deenotes the shortest allowed substring
+#	an int n that denotes the shortest allowed substring
 #	a list of strings to find the average common substring count of
 #	a summary string list used for comparison against the test string
 #Return:
@@ -90,12 +95,12 @@ def getAvgComSubstrCount(n, testList, summary):
 		substrSummary.extend(getAllSubstr(n, item))
 	avgComSubstrCount = 0
 	for testStr in testList:
-		avgComSubstrCount += getComSubstrCount(testStr, substrSummary)
+		avgComSubstrCount += getComSubstrCount(n, testStr, substrSummary)
 	avgComSubstrCount = avgComSubstrCount/len(testList)
 	return avgComSubstrCount
 
 #Input:
-#	an int n that deenotes the shortest allowed substring
+#	an int n that denotes the shortest allowed substring
 #	a test string to find the common substring count of when compared to the substring summary
 #	the list of all of the substrings for the summary
 #Return:
@@ -115,6 +120,5 @@ def getComSubstrCount(n, testStr, substrSummary):
 #	a list of all the substrings of at least length n in the input string
 def getAllSubstr(n, inputStr):
 	length = len(inputStr)
-	return [inputStr[i:j + 1] for i in xrange(length) for j in xrange(i + (n-1),length)]
-
+	return [inputStr[i:j + 1] for i in range(length) for j in range(i + (n-1),length)]
 
