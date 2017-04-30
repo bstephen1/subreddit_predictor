afbc0eebb463f6f306ea25fd8a34132a00699083
bstephen1 <bstep1@umbc.edu>
2017-04-30 19:40:08 -0400

    added data files

---
+++ reddit_scraper/data/catsstandingup50.jl
@@ -0,0 +1,50 @@
+{"TFLAIR": null, "FLAIRS": ["Cat."], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 10100, "TOTAL": 554, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 4718, "TOTAL": 164, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["Cat.  ", "Cat.                   "], "SUBREDDIT": "CatsStandingUp", "SCORE": 4503, "TOTAL": 172, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["[removed]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 4364, "TOTAL": 146, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["[removed]", "[removed]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 4605, "TOTAL": 523, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["[deleted]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 4337, "TOTAL": 149, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 4426, "TOTAL": 166, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 4846, "TOTAL": 147, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 4738, "TOTAL": 130, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5537, "TOTAL": 267, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5468, "TOTAL": 166, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5433, "TOTAL": 137, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5131, "TOTAL": 187, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": ["Cat."], "TITLE": "Cat.", "COMMENTS": ["[removed]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 5326, "TOTAL": 252, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5149, "TOTAL": 185, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5367, "TOTAL": 219, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5438, "TOTAL": 1471, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3619, "TOTAL": 73, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3544, "TOTAL": 151, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3510, "TOTAL": 93, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3576, "TOTAL": 188, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3525, "TOTAL": 107, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3482, "TOTAL": 114, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3536, "TOTAL": 121, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["[deleted]", "[removed]", "[deleted]", "[deleted]", "[deleted]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 3448, "TOTAL": 414, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3889, "TOTAL": 141, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3627, "TOTAL": 72, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3614, "TOTAL": 118, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["[removed]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 3712, "TOTAL": 121, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["Cat.  ", "[deleted]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 3873, "TOTAL": 135, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["[removed]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 3802, "TOTAL": 132, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3798, "TOTAL": 190, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3858, "TOTAL": 424, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3917, "TOTAL": 115, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 4303, "TOTAL": 219, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 4114, "TOTAL": 129, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["Cat.   "], "SUBREDDIT": "CatsStandingUp", "SCORE": 3982, "TOTAL": 122, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 4187, "TOTAL": 132, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["[deleted]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 4030, "TOTAL": 137, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["Cat.  "], "SUBREDDIT": "CatsStandingUp", "SCORE": 4310, "TOTAL": 215, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 3992, "TOTAL": 202, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 4112, "TOTAL": 207, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 6873, "TOTAL": 291, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 8764, "TOTAL": 536, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5611, "TOTAL": 200, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5785, "TOTAL": 350, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 5874, "TOTAL": 223, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["[deleted]", "[removed]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 6809, "TOTAL": 260, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": [], "SUBREDDIT": "CatsStandingUp", "SCORE": 6576, "TOTAL": 280, "TYPE": 0}
+{"TFLAIR": null, "FLAIRS": [], "TITLE": "Cat.", "COMMENTS": ["[removed]"], "SUBREDDIT": "CatsStandingUp", "SCORE": 8919, "TOTAL": 331, "TYPE": 0}
