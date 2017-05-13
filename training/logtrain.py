afb972fe529a0e59337359cae01c9ca33af9cb26
bstephen1 <bstep1@umbc.edu>
2017-05-13 19:43:53 -0400

    Add files via upload
    more robust. can handle a separate testing file and command args, etc

--- training/logtrain.py
+++ training/logtrain.py
@@ -2,36 +2,63 @@ from sklearn import linear_model
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import train_test_split
 import json
+import sys
 
 #this file reads in formatted feature vectors from "training.jl" 
 #and trains it using logistic regression
 
-#NOTE: this file is completely untested
+#usage: logtrain.py [trainingfile.jl] [testingfile.jl]
+#first parameter is required. second is optional 
+argc = len(sys.argv)
+argv = sys.argv
+if argc > 3 or argc < 2 :
+	print("Usage: logtrain.py [trainingfile.jl] [testingfile.jl]")
+	sys.exit()
 
 #set up the model
 log = linear_model.LogisticRegression()
 
 #get the data
 #raw format has class label as last element
-posts = []
-with open("askderp.jl") as f :
-	for line in f :
-		posts.append(json.loads(line))
+def getData(f) :
+	posts = []
+	with open(argv[1]) as f :
+		for line in f :
+			posts.append(json.loads(line))
+	#convert raw format to X and Y vectors
+	X_data = []
+	Y_data = []
+	for post in posts :
+		X_data.append(post[:-1])
+		Y_data.append(post[-1])
+	return [X_data, Y_data]
 
-#convert raw format to X and Y vectors
-X_data = []
-Y_data = []
-for post in posts :
-	X_data.append(post[:-1])
-	Y_data.append(post[-1])
+#get the training data
+train = getData(argv[1])
+X_data = train[0]
+Y_data = train[1]	
 
 #set a portion of the training data as testing samples
-X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size=.1, random_state=1)
+#only if no separate test file is given
+if argc == 2 :
+	X_train, X_test, Y_train, Y_test = train_test_split(X_data, Y_data, test_size=.1, random_state=1)
+
+else :
+	test = getData(argv[2])
+	X_train = X_data
+	Y_train = Y_data
+	X_test = test[0]
+	Y_test = test[1]
+	
 
 #train
 model = log.fit(X_train, Y_train)
 
-#print stats
+#record results to file 
+f = open("results.txt", "a")
+extra = argv[2] if argc == 3 else "None"
+f.write("Train: %s :: Test: %s :: Acc: %f\n" % (argv[1], extra, log.score(X_test,Y_test)))
+
 #prints actual class label vs prediction
 for x in range(len(Y_test)) :
 	print("Actual: %f :: Predicted: %f :: x: %d" % (Y_test[x], model.predict([X_test[x]]), x))
