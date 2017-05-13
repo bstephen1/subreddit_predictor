838ec9f446909e65bffebffeef7479f6b74237f8
edaniel1 <edaniel1@umbc.edu>
2017-05-13 13:32:24 -0400

    Completed ParamExtractor2.py
    Still need to make it more readable and add comments, but it SHOULD be usable.
    see ReadMe

--- extract_parameters/ParamExtractor2.py
+++ extract_parameters/ParamExtractor2.py
@@ -1,11 +1,44 @@
 from textblob import TextBlob
+import pickle
 import json
 import sys
 from sklearn.feature_extraction.text import CountVectorizer
 
+def main():
 
+	fileNameFile = sys.argv[1]
+	value = int(sys.argv[2])
+	#print(type(sys.argv[1:]))
+	
+	fileNames = []
+	
+	for line in open(fileNameFile):
+		fileNames.append('data\\' + line.rstrip('\n'))
+	print(fileNames)
+	
+	trainFiles(fileNames, value)
 
-def getTrainingData(fileName):
+def trainFiles(fileNames, num):
+
+	trainingData, titleVectorizer, tFlairVectorizer, flairsVectorizer, commentsVectorizer = getTrainingData(fileNames[:num])
+
+	ID = ""
+	
+	for index in range(num):
+		ID += fileNames[index][5:-3]
+	
+	for subIndex in range(len(trainingData)):
+		for postIndex in range(0,len(trainingData[subIndex])):
+			trainingData[subIndex][postIndex] = int(trainingData[subIndex][postIndex])
+	writeTrainingData(trainingData, ID)
+	writeVectorizerLibrary(titleVectorizer, tFlairVectorizer, flairsVectorizer, commentsVectorizer, ID)
+
+def getTrainingData(fileNames):
+
+	if (type(fileNames) is not list):
+		print("Parameter for funciton getTrainingData() must be a list of filenames.")
+		print("Now exiting...")
+		sys.exit()
 
 	#Contains a dictionary of the subreddits and their corresponding list index
 	subRedditDict = {}
@@ -20,8 +53,15 @@ def getTrainingData(fileName):
 	#	allSubs.append(getFileDictList(sys.argv[i]))
 	
 	#all of the posts in one list
-	disorgAllPosts = getFileDictList(fileName)
-
+	disorgAllPosts = []
+	for fileName in fileNames:
+		try:
+			disorgAllPosts.extend(getFileDictList(fileName))
+		except:
+			print("Error reading file. Name of file is: ")
+			#print(file)
+			sys.exit()
+			
 	#fills allSubs with the subreddits and their posts
 	currentIndex = 0
 	for post in disorgAllPosts:
@@ -41,14 +81,18 @@ def getTrainingData(fileName):
 	for subReddit in allSubs:
 		for post in subReddit:
 			vectorizerFitDataTitle.append(post["TITLE"])
+			if(post["TFLAIR"] == None):
+				post["TFLAIR"] = "null"
 			vectorizerFitDataTFlair.append(post["TFLAIR"])
+			if(post["FLAIRS"] == []):
+				post["FLAIRS"] = ["null"]
 			vectorizerFitDataFlairs.extend(post["FLAIRS"])
 			vectorizerFitDataComments.extend(post["COMMENTS"])
 			
-	titleVectorizer = CountVectorizer(stop_words='english')
-	tFlairVectorizer = CountVectorizer(stop_words='english')
-	flairsVectorizer = CountVectorizer(stop_words='english')
-	commentsVectorizer = CountVectorizer(stop_words='english')
+	titleVectorizer = CountVectorizer(stop_words = 'english')#, max_df=.8, min_df=.001)
+	tFlairVectorizer = CountVectorizer(stop_words = 'english')
+	flairsVectorizer = CountVectorizer(stop_words = 'english')
+	commentsVectorizer = CountVectorizer(stop_words = 'english')#, max_df=.8, min_df=.001)
 	
 	titleVectorizer.fit(vectorizerFitDataTitle)
 	tFlairVectorizer.fit(vectorizerFitDataTFlair)
@@ -63,31 +107,44 @@ def getTrainingData(fileName):
 			
 			postTitle = []
 			postTitle.append(post["TITLE"])
-			postData.extend(titleVectorizer.transform(postTitle))
+			data = titleVectorizer.transform(postTitle)
+			denseData = data.toarray()
+			#print(denseData)
+			postData.extend(denseData[0])
 			
 			postTFlair = []
 			postTFlair.append(post["TFLAIR"])
-			postData.extend(tFlairVectorizer.transform(postTFlair))
+			data = tFlairVectorizer.transform(postTFlair)
+			denseData = data.toarray()
+			#print(denseData)
+			postData.extend(denseData[0])
 			
 			postFlairsString = ""
 			for flair in post["FLAIRS"]:
 				postFlairsString += " " + flair
 			postFlairs = []
 			postFlairs.append(postFlairsString)
-			postData.extend(flairsVectorizer.transform(postFlairs))
+			data = flairsVectorizer.transform(postFlairs)
+			denseData = data.toarray()
+			#print(denseData)
+			postData.extend(denseData[0])
+			
 			
 			postCommentsString = ""
 			for comment in post["COMMENTS"]:
-				postCommentsString += " " + flair
+				postCommentsString = postCommentsString + " " + comment
 			postComments = []
 			postComments.append(postCommentsString)
-			postData.extend(commentsVectorizer.transform(post["COMMENTS"]))
+			data = commentsVectorizer.transform(postComments)
+			denseData = data.toarray()
+			#print(denseData)
+			postData.extend(denseData[0])
 			
 			postData.append(subRedditDict[post["SUBREDDIT"]])
 			
 			trainingData.append(postData)
 			
-			return trainingData, titleVectorizer, tFlairVectorizer, flairsVectorizer, commentsVectorizer
+	return trainingData, titleVectorizer, tFlairVectorizer, flairsVectorizer, commentsVectorizer
 
 
 
@@ -106,28 +163,59 @@ def getPostParams(post, titleVectorizer, tFlairVectorizer, flairsVectorizer, com
 	
 	postTitle = []
 	postTitle.append(post["TITLE"])
-	postData.extend(titleVectorizer.transform(postTitle))
-			
+	data = titleVectorizer.transform(postTitle)
+	denseData = data.toarray()
+	postData.extend(denseData[0])
+	
 	postTFlair = []
 	postTFlair.append(post["TFLAIR"])
-	postData.extend(tFlairVectorizer.transform(postTFlair))
-			
+	data = tFlairVectorizer.transform(postTFlair)
+	denseData = data.toarray()
+	postData.extend(denseData[0])
+	
 	postFlairsString = ""
 	for flair in post["FLAIRS"]:
 		postFlairsString += " " + flair
 	postFlairs = []
 	postFlairs.append(postFlairsString)
-	postData.extend(flairsVectorizer.transform(postFlairs))
-			
+	data = flairsVectorizer.transform(postFlairs)
+	denseData = data.toarray()
+	postData.extend(denseData[0])
+	
+	
 	postCommentsString = ""
 	for comment in post["COMMENTS"]:
-		postCommentsString += " " + flair
+		postCommentsString = postCommentsString + " " + comment
 	postComments = []
 	postComments.append(postCommentsString)
-	postData.extend(commentsVectorizer.transform(post["COMMENTS"]))
-			
+	data = commentsVectorizer.transform(postComments)
+	denseData = data.toarray()
+	postData.extend(denseData[0])
+	
 	postData.append(subRedditDict[post["SUBREDDIT"]])
 	
 	return postData
 	
-print(getTrainingData(sys.argv[1])[0])
+def writeTrainingData(trainingData, ID):
+
+	outfileName = 'trainingData' + ID + '.jl'
+
+	with open(outfileName, 'w') as outfile:
+		for post in trainingData:
+			json.dump(post, outfile)
+			outfile.write('\n')
+	
+	return
+
+def writeVectorizerLibrary(titleVectorizer, tFlairVectorizer, flairsVectorizer, commentsVectorizer, ID):
+
+	outfileName = 'vectorizerLibraries' + ID + '.pkl'
+
+	with open(outfileName, 'w') as outfile:
+		pickle.dump(titleVectorizer, outfile)
+		pickle.dump(tFlairVectorizer, outfile)
+		pickle.dump(flairsVectorizer, outfile)
+		pickle.dump(commentsVectorizer, outfile)
+		
+
+main()
---
+++ extract_parameters/ReadMe.txt
@@ -0,0 +1,7 @@
+ReadMe for ParamExtractor2.py
+
+Still need to make it more readable and add comments, but it SHOULD be usable.
+To use, give it a file with the filenames and an integer.
+The program must be located in the directory above the data files.
+The directory containing the data files must be called "data".
+The program will create training data based on the first x files, where x is the integer you passed in.
